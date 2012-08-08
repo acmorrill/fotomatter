3305d981576619a9c1c9025537012bcab0eb31e3
Drew Morrill <acmorrill@gmail.com>
2012-08-07 20:10:05 -0600

    Changes

--- themes/default/theme_config.php
+++ themes/default/theme_config.php
@@ -5,6 +5,9 @@ $theme_config = array(
 	'theme_controller_action_layouts' => array(
 		'SitePages' => array(
 			'landing_page' => 'landing'
+		),
+		'Themes' => array(
+			'choose_gallery' => 'gallery_landing'
 		)
 	) 
 );
