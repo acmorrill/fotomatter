db2a549a6bc5424febf5d4d599a175ddc42d74a2
Adam Holsinger <adamdude828@gmail.com>
2012-09-08 18:17:35 -0600

    Changes

--- models/old_photo.php
+++ models/old_photo.php
@@ -79,3 +79,4 @@ class OldPhoto extends AppModel {
           $this->save($this->data);
      }
 }
+*/
--- tests/fototestcase.php
+++ tests/fototestcase.php
@@ -1,41 +1,52 @@
 <?php
 class fototestcase extends CakeTestCase {
     
+    public $tables = array();
+    
+    public $sources = array();
+    
+    public $live_db = array();
+    
     public function before($method) {
-        $default_db_data = @$db =& ConnectionManager::getDataSource('server_global');
-        $output_command_global = "mysqldump -u {$default_db_data->config['login']} -p{$default_db_data->config['password']} {$default_db_data->config['database']}";
-        
-        $default_db_data = @$db =& ConnectionManager::getDataSource('default');
-        $output_command = "mysql -u {$default_db_data->config['login']} -p{$default_db_data->config['password']} {$default_db_data->config['database']}";
-        
-        @$db =& ConnectionManager::getDataSource('test');
-        $input_command = "mysql -u {$db->config['login']} -p{$db->config['password']} {$db->config['database']}";
-        
-        $tmp = "/tmp/db_dump";
-        $command = $output_command . " > " . $tmp . " && " . $input_command . " < " . $tmp;
-        debug($command); die();
-        
-        
-        debug($command); die();
-        exec($command);
-        
-        $command = $output_command . " > " . $input_command;
-        exec($command); 
-        
-        die();
-        
-        
-        
-        
-        
-       /* $all_models = App::objects('model');
-        foreach ($all_models as $model) {
-            $table_sql = $this->_get_table($model);
-            $model->setDataSource('test_suite');
-           
+        if (empty($this->tables)) {
+            $all_sources = & ConnectionManager::sourceList();
+            foreach ($all_sources as $source) {
+                $this->sources[$source] = ConnectionManager::getDataSource($source);
+                $this->tables[$source] = $this->sources[$source]->listSources();
+            
+                $config = $this->sources[$source]->config;
+                foreach ($this->tables[$source] as $table) {
+                    $mysql_dump_command = "mysqldump -u {$config['login']} -p{$config['password']} {$config['database']} $table --skip-comments";     
+                    exec($mysql_dump_command, $this->live_db[$source][$table]);
+                    $this->live_db[$source][$table] = implode("\n", $this->live_db[$source][$table]);
+                }
+            }
         }
         
-        $testDbAvailable = in_array('test', array_keys(ConnectionManager::enumConnectionObjects()));
+        //get the test datasource and then drop all databases
+        @$test_db =& ConnectionManager::getDataSource('test');
+        $resource = $this->connect($test_db->config);
+        $all_sources = & ConnectionManager::sourceList();
+        
+        foreach ($all_sources as $source) {
+            if ($source == 'test') continue;
+            
+            foreach ($this->tables[$source] as $table) {
+                if (mysql_query("DROP TABLE if exists $table", $resource) == false) {
+                    die('problem clearing test db');
+                }
+                debug($this->live_db); die();
+                debug(mysql_query($this->live_db[$source][$table], $resource));
+            }
+        }
+        
+        //build db
+        
+        
+        
+        
+        
+        /*$testDbAvailable = in_array('test', array_keys(ConnectionManager::enumConnectionObjects()));
 
         if ($testDbAvailable) {
                 // Try for test DB
@@ -53,6 +64,17 @@ class fototestcase extends CakeTestCase {
         $this->db->cacheSources  = false;
 
      //   ClassRegistry::config(array('ds' => 'test_suite')); */
+        
+        
+    }
+    
+    private function connect($config) {
+        $res = mysql_connect($config['host'], $config['login'], $config['password']);
+        mysql_select_db($config['database'], $res);
+        return $res;
+    }
+    
+    private function _populate_tables() {
         
         
     }
