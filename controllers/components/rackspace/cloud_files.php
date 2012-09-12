e3b33f130362eb9c7dc12f1bd8ca44268f1b9044
Adam Holsinger <adamdude828@gmail.com>
2012-09-11 21:50:15 -0600

    Changes

--- controllers/components/rackspace/cloud_files.php
+++ controllers/components/rackspace/cloud_files.php
@@ -119,6 +119,7 @@ class CloudFilesComponent extends RackspaceObj {
     }
     
     public function put_object($object_name, $file_path, $mime_type, $container=false) {
+        debug($file_path); 
 		if ($container === false) {
                     $container = $this->_getContainerName();
                     if ($container === false) return false;
--- tests/fototestcase.php
+++ tests/fototestcase.php
@@ -9,7 +9,49 @@ class fototestcase extends CakeTestCase {
     
     public $source_list = array();
     
-    public function startTest($method) {
+    public $reload_tables_after_method = true;
+    
+    public $exclude_these_tables = array();
+    
+    public $include_these_tables = array();
+    
+    public function before() {
+        @$test_db =& ConnectionManager::getDataSource('test');
+        
+        ConnectionManager::create('test_suite', $test_db->config);
+        $test_db->cacheSources = false;
+        ClassRegistry::config(array('ds' => 'test_suite')); 
+    }
+    
+    public function start() {
+        if ($this->reload_tables_after_method === false) {
+            $this->reload_start();
+        }
+    }
+    
+    public function end() {
+        if ($this->reload_tables_after_method === false) {
+            $this->reload_end();
+        }
+    }
+    
+    public function startTest() {
+        if ($this->reload_tables_after_method) {
+            $this->reload_start();
+        }
+    }
+    
+    public function endTest() {
+        if ($this->reload_tables_after_method) {
+            $this->reload_end();
+        }
+    }
+    
+    public function reload_start() {
+        if (empty($this->include_these_tables) == false && empty($this->exclude_these_tables) == false) {
+            debug("used include tables and exclude tables at the same time.");
+            die();
+        }
         
         if (empty($this->source_list)) {
             $db_config = new DATABASE_CONFIG();
@@ -23,9 +65,21 @@ class fototestcase extends CakeTestCase {
             foreach ($this->source_list as $source) {
                 $this->sources[$source] = ConnectionManager::getDataSource($source);
                 $this->tables[$source] = $this->sources[$source]->listSources();
-            
+                
                 $config = $this->sources[$source]->config;
-                foreach ($this->tables[$source] as $table) {
+                foreach ($this->tables[$source] as $table_key => $table) {
+                    if (empty($this->exclude_these_tables) === false) {
+                        if (in_array($table, $this->exclude_these_tables)) {
+                            unset($this->tables[$source][$table_key]);
+                            continue;
+                        }
+                    } elseif (empty($this->include_these_tables) === false) {
+                        if (in_array($table, $this->include_these_tables) == false) {
+                            unset($this->tables[$source][$table_key]);
+                            continue;
+                        }
+                    }
+                    
                     $mysql_dump_command = "mysqldump -u {$config['login']} -p{$config['password']} {$config['database']} $table --compact";     
                     exec($mysql_dump_command, $this->live_db[$source][$table]);
                     $this->live_db[$source][$table] = explode(";\n", implode("\n", $this->live_db[$source][$table]));
@@ -53,8 +107,8 @@ class fototestcase extends CakeTestCase {
         ClassRegistry::config(array('ds' => 'test_suite')); 
     }
     
-    public function endTest() {
-      /*  @$test_db =& ConnectionManager::getDataSource('test');
+    public function reload_end() {
+        @$test_db =& ConnectionManager::getDataSource('test');
         $resource = $this->connect($test_db->config);
         
         foreach ($this->source_list as $source) {
@@ -64,7 +118,7 @@ class fototestcase extends CakeTestCase {
                     die('problem clearing test db');
                 }
             }
-        } */
+        }
     }
     
     private function connect($config) {
