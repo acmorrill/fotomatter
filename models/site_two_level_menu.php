4f35e3bc796062316f6aaf22055efb475ca55285
Drew Morrill <acmorrill@gmail.com>
2012-11-10 23:11:52 -0700

    mostly done with the two level menu editor in the admin

--- models/site_two_level_menu.php
+++ models/site_two_level_menu.php
@@ -28,4 +28,26 @@ class SiteTwoLevelMenu extends AppModel {
 		)
 	);
 	
+	public function beforeDelete() {
+		$site_two_level_menu = $this->find('first', array(
+			'conditions' => array(
+				'SiteTwoLevelMenu.id' => $this->id
+			),
+			'contain' => false
+		));
+		
+		if ($site_two_level_menu['SiteTwoLevelMenu']['external_model'] == 'SiteTwoLevelMenuContainer') {
+			$site_two_level_menu_container_id = $site_two_level_menu['SiteTwoLevelMenu']['external_id'];
+			if (!$this->SiteTwoLevelMenuContainer->delete($site_two_level_menu_container_id)) {
+				$this->major_error('failed to delete site_two_level_menu_container in beforeDelete of SiteTwoLevelMenu');
+			}
+		}
+		
+		return true;
+	}
+	
+	public function afterDelete() {
+//		$this->log($this->data, 'afterDelete');
+	}
+	
 }
