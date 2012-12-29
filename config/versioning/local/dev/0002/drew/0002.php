03b474b59856da2a4bacbe1188b6354de54218af
Drew Morrill <acmorrill@gmail.com>
2012-12-28 17:42:39 -0700

    Changes

--- config/versioning/local/dev/0002/drew/0002.php
+++ config/versioning/local/dev/0002/drew/0002.php
@@ -3,5 +3,5 @@
 $sqls = array();
 $functions = array();
 
-$sqls[] = "UPDATE  `fotomatter`.`site_settings` SET  `value` =  'white_slider' WHERE  `site_settings`.`name` ='current_theme';";
+$sqls[] = "UPDATE  `site_settings` SET  `value` =  'white_slider' WHERE  `site_settings`.`name` ='current_theme';";
 
