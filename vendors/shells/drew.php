c476ddaef8b3995e000f3393f08514581b97fa65
Drew Morrill <acmorrill@gmail.com>
2013-04-23 19:13:11 -0600

    Changes

--- vendors/shells/drew.php
+++ vendors/shells/drew.php
@@ -38,5 +38,146 @@ class DrewShell extends Shell {
 		}
 	}
 	
+	public function calc_phase_equity() {
+		$phase_1 = array(
+			'a' => array(
+				'start_equity' => 37.5,
+//				'hours' => 18.08,
+				'hours' => 15,
+			),
+			'b' => array(
+				'start_equity' => 37.5,
+				'hours' => 10.83,
+			),
+			'c' => array(
+				'start_equity' => 25,
+				'hours' => 11.91,
+			),
+		);
+		$this->calc_equity($phase_1, .7);
+		debug($phase_1);
+		
+		
+		$phase_2 = array(
+			'a' => array(
+				'start_equity' => 37.5,
+//				'hours' => 18.08,
+				'hours' => 15,
+			),
+			'b' => array(
+				'start_equity' => 37.5,
+				'hours' => 10.83,
+			),
+			'c' => array(
+				'start_equity' => 25,
+				'hours' => 11.91,
+			),
+		);
+		$this->calc_equity($phase_2, .15);
+		debug($phase_2);
+		
+		
+		$phase_3 = array(
+			'a' => array(
+				'start_equity' => 37.5,
+//				'hours' => 18.08,
+				'hours' => 15,
+			),
+			'b' => array(
+				'start_equity' => 37.5,
+				'hours' => 10.83,
+			),
+			'c' => array(
+				'start_equity' => 25,
+				'hours' => 11.91,
+			),
+		);
+		$this->calc_equity($phase_3, .15);
+		debug($phase_3);
+	}
+	
+	
+	public function calc_equity(&$people, $phase_percent) {
+		// get lost equity
+		foreach ($people as $name => &$person) {
+			$person['lost_equity'] = $this->equity_lost($person['hours'], $person['start_equity']);
+			$person['equity_kept'] = $this->equity_kept($person['hours'], $person['start_equity']);
+		}
+		
+	
+		
+		$people['a']['gained'] = 0;
+		$people['a']['gained'] += $this->calc_gained_equity($people['a']['hours'], $people['c']['hours'], $people['b']['lost_equity']);
+		$people['a']['gained'] += $this->calc_gained_equity($people['a']['hours'], $people['b']['hours'], $people['c']['lost_equity']);
+		$people['a']['total_equity'] = $people['a']['equity_kept'] + $people['a']['gained'];
+		
+		$people['b']['gained'] = 0;
+		$people['b']['gained'] += $this->calc_gained_equity($people['b']['hours'], $people['c']['hours'], $people['a']['lost_equity']);
+		$people['b']['gained'] += $this->calc_gained_equity($people['b']['hours'], $people['a']['hours'], $people['c']['lost_equity']);
+		$people['b']['total_equity'] = $people['b']['equity_kept'] + $people['b']['gained'];
+		
+		$people['c']['gained'] = 0;
+		$people['c']['gained'] += $this->calc_gained_equity($people['c']['hours'], $people['b']['hours'], $people['a']['lost_equity']);
+		$people['c']['gained'] += $this->calc_gained_equity($people['c']['hours'], $people['a']['hours'], $people['b']['lost_equity']);
+		$people['c']['total_equity'] = $people['c']['equity_kept'] + $people['c']['gained'];
+		
+		foreach ($people as &$person) {
+			$person['phase_equity'] = $person['total_equity'] * $phase_percent;
+		}
+		
+		
+//		$all_total = $people['a']['total_equity'] + $people['b']['total_equity'] + $people['c']['total_equity'];
+		
+		
+		// divy up lost equity
+//		foreach ($people as $name => $person) {
+//			$other_keys = $this->get_other_two_people_keys($name, $people);
+//			
+//			$gained = 0;
+//			foreach($other_keys as $curr_key) {
+//				$gained += $this->calc_gained_equity($one_hours, $two_hours, $equity_up_for_grabs);
+//			}
+//		}
+		
+		
+		
+		
+	}
+	
+	public function calc_gained_equity($one_hours, $two_hours, $equity_up_for_grabs) {
+		if ($equity_up_for_grabs <= 0) {
+			return 0;
+		}
+		
+		$normalize_val = 5;
+		
+		return ( ( ($one_hours - $normalize_val) / ( ($one_hours - $normalize_val) + ($two_hours - $normalize_val) ) ) ) * $equity_up_for_grabs;
+	}
+	
+	public function get_other_two_people_keys($me, $array) {
+		$other_keys = array();
+		foreach ($array as $key => $current) {
+			if ($key != $me) {
+				$other_keys[] = $key;
+			}
+		}
+		
+		return $other_keys;
+	}
+		
+	
+	
+	public function equity_kept($hours, $equity) {
+		if ($hours > 15) {
+			return $equity;
+		}
+		
+		return (((7 * $hours) - 5) / 100) * $equity;
+	}
+	
+	public function equity_lost($hours, $equity) {
+		return $equity - $this->equity_kept($hours, $equity);
+	}
+	
 	
 }
