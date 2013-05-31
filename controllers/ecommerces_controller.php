b37c489bfd5e9bc0bb00d27a171edfb11a340661
Andrew Morrill <acmorrill@gmail.com>
2013-05-30 21:51:08 -0600

    Changes

--- controllers/ecommerces_controller.php
+++ controllers/ecommerces_controller.php
@@ -324,8 +324,8 @@ class EcommercesController extends AppController {
 	}
 	
 	public function view_cart() {
-		$this->Cart->create_fake_cart_items(); // DREW TODO - delete this line
-//		$this->Cart->create_fake_cart_items_laptop(); // DREW TODO - delete this line
+//		$this->Cart->create_fake_cart_items(); // DREW TODO - delete this line
+		$this->Cart->create_fake_cart_items_laptop(); // DREW TODO - delete this line
 		
 		$this->ThemeRenderer->render($this);
 	}
