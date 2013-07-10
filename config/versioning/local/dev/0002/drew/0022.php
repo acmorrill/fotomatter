d9453942ba98f923f0245c1af2a65e8afddc4d6f
Andrew Morrill <acmorrill@gmail.com>
2013-07-09 18:19:27 -0600

    Changes

--- config/versioning/local/dev/0002/drew/0022.php
+++ config/versioning/local/dev/0002/drew/0022.php
@@ -5,6 +5,6 @@ $functions = array();
 
 
 
-$sqls[] = "ALTER TABLE  `authnet_orders` ADD  `full_response` TEXT NOT NULL AFTER  `created`";
+$sqls[] = "ALTER TABLE  `authnet_orders` ADD  `full_parsed_response` TEXT NOT NULL AFTER  `created`";
 $sqls[] = "ALTER TABLE  `authnet_orders` ADD  `transaction_id` INT NOT NULL AFTER  `full_parsed_response`";
 $sqls[] = "ALTER TABLE  `authnet_orders` CHANGE  `transaction_id`  `transaction_id` BIGINT( 11 ) NOT NULL";
