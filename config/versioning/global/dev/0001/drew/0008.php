9e54007b8039a7a361e993dc2bf376b7714e5582
Drew Morrill <acmorrill@gmail.com>
2013-08-14 22:20:32 -0600

    Changes

---
+++ config/versioning/global/dev/0001/drew/0008.php
@@ -0,0 +1,15 @@
+<?php
+
+$sqls = array();
+$functions = array();
+
+$sqls[] = "CREATE TABLE IF NOT EXISTS `welcome_hashes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hash` text NOT NULL,
+  `account_id` char(36) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
+$sqls[] = "ALTER TABLE  `welcome_hashes` CHANGE  `hash`  `hash` CHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL";
+$sqls[] = "ALTER TABLE  `fotomatter_global`.`welcome_hashes` ADD UNIQUE (`account_id`)";
+$sqls[] = "ALTER TABLE  `fotomatter_global`.`welcome_hashes` ADD UNIQUE (`hash`)";
--- vendors/shells/build.php
+++ vendors/shells/build.php
@@ -9,28 +9,34 @@ class BuildShell extends Shell {
      * 3. dns has already been taken care of.
      */
     public function now() {
-	//so the idea is that customer mangement will initiate this process, and pass the root password,
-	//check to make sure it works
-	if (empty($this->params[0]) === false) {
-	    $this->error("Please provide root mysql password.");
-	    exit(1);
-	}
-	
-	$mysql_connection_resource = @mysql_connect("localhost", "root", $this->params[0]);
-	if ($mysql_connection_resource === false) {
-	    $this->error("Mysql root password was provided but, could not connect");
-	    exit(1);
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		//so the idea is that customer mangement will initiate this process, and pass the root password,
+		//check to make sure it works
+		if (empty($this->params[0]) === false) {
+			$this->error("Please provide root mysql password.");
+			exit(1);
+		}
+
+		$mysql_connection_resource = @mysql_connect("localhost", "root", $this->params[0]);
+		if ($mysql_connection_resource === false) {
+			$this->error("Mysql root password was provided but, could not connect");
+			exit(1);
+		}
     }
+	
+	
+	public function get_welcome_hash() {
+		$this->GlobalWelcomeHash = ClassRegistry::init('GlobalWelcomeHash');
+		
+		try {
+			$new_hash = $this->GlobalWelcomeHash->create_new_hash_entry();
+			echo $new_hash;
+			exit(0);
+		} catch (Exception $e) {
+			exit(1);
+		}
+	}
     
     
+	
+	
 }
