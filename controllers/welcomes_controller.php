7e849cf260b80054800995a178d14abe0de1e9ac
Drew Morrill <acmorrill@gmail.com>
2013-08-15 22:39:41 -0600

    Changes

---
+++ controllers/welcomes_controller.php
@@ -0,0 +1,12 @@
+<?php
+class WelocomesController extends AppController {
+    public $name = 'Welocomes';
+	
+	public function  beforeFilter() {
+		parent::beforeFilter();
+	}
+	
+    public function login($authentication) {
+		
+	}
+}
