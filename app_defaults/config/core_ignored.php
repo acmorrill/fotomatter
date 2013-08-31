18b313319643c6c1c67209b994dd57fdd9bb957a
Adam Holsinger <adamdude828@gmail.com>
2013-08-30 22:25:52 -0600

    chahnges

--- app_defaults/config/core_ignored.php
+++ app_defaults/config/core_ignored.php
@@ -1,4 +1,6 @@
 <?php
 Configure::write('file_folder_default_user', 'server_stats');
 
-//Configure::write('OVERLORD_URL', 'overlord.local.net');
+Configure::write('OVERLORD_URL', 'overlord.local.net');
+
+Configure::write('debug', 2);
