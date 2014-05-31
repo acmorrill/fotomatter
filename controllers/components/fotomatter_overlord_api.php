0e0481dcbacaef7fb0123edecec92bc30bd1690d
Andrew Morrill <acmorrill@gmail.com>
2014-05-31 14:09:39 -0600

    changes

--- controllers/components/fotomatter_overlord_api.php
+++ controllers/components/fotomatter_overlord_api.php
@@ -16,15 +16,20 @@ class FotoMatterOverlordApi extends Object {
 		
 
 		$ch = curl_init();
+		// DREW TODO - make this request over SSL
 		curl_setopt($ch, CURLOPT_URL , $url_to_use);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		//curl_setopt($ch, CURLINFO_HEADER_OUT, true);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
 			'Content-Type: application/json',
 			'API_SIGNATURE: '.$request['Access']['signature']
 		));
 		$json_response = curl_exec($ch);
+		
+//		$curl_info = curl_getinfo($ch);
+//		$this->log($curl_info, 'curl_info');
 		
 		if ($json_response === false) {
 			$curl_error = curl_error($ch);
