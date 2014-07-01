99027035107811c1c368aae228f692e24cfc09ce
Kent Gigger <giggerkent@gmail.com>
2014-06-30 22:51:47 -0600

    I created the cart page and the login page. All styled too YEAH!

--- themes/large_image_gray_bar_licky/theme_config.php
+++ themes/large_image_gray_bar_licky/theme_config.php
@@ -39,7 +39,7 @@ $theme_config = array(
 				'view' => 'view_cart',
 			),
 			'checkout_login_or_guest' => array(
-				'layout' => 'custom_page',
+				'layout' => 'login_or_guest',
 				'view' => 'checkout_login_or_guest',
 			),
 			'checkout_get_address' => array(
