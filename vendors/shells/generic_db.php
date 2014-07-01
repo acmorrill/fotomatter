85138de4a0bbc2bc89c1f917a06e55d00cd89cef
Andrew Morrill <acmorrill@gmail.com>
2014-06-30 18:40:49 -0600

    Changes

--- vendors/shells/generic_db.php
+++ vendors/shells/generic_db.php
@@ -80,9 +80,11 @@ class GenericDbShell extends Shell {
 	protected function _update($is_global) {
 		if ($is_global == false) {
 			ClassRegistry::init("SiteSetting");
-			$datasource = ConnectionManager::getDataSource('default');
-	                $datasource->cacheSources = false;
+		} else {
+			ClassRegistry::init("ServerSetting");
 		}
+		$datasource = ConnectionManager::getDataSource('default');
+                $datasource->cacheSources = false;
 		Configure::write('Cache.disable', true);
 		$this->_connect_db();
 		
