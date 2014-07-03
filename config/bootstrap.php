355dc65d5f1364f4b319a99e6732552128817fae
Andrew Morrill <acmorrill@gmail.com>
2014-07-02 22:53:10 -0600

    Changes

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -54,6 +54,7 @@ define("HELP_TOUR_ENGLISH_TEXT", 'Get Help With This Page');
 define("SITE_SETTINGS_APC_CACHE_TTL", 28800); // 8  hours
 define("SITE_SETTINGS_APC_DEFAULT_KEY", 'USE_THE_DEFAULT'); // the string to store for site settings apc to indicate to use the default setting
 define("FRONTEND_VIEW_CACHING_STRTOTIME_TTL", '1 week'); // how long to keep view caching
+define("VIEW_CACHE_PATH", ROOT . '/tmp/cache/views');
 
 date_default_timezone_set('America/Denver');
 
@@ -124,7 +125,6 @@ define('MAX_UPLOAD_SIZE_MEGS', 5);
 define('FREE_MAX_RES', 2000);
 define("USE_CACHE_SPEED", true);
 
-define("VIEW_CACHE_PATH", ROOT . '/tmp/cache/views');
 
 //path to overlord. (so it can change for development)
 Configure::write('OVERLORD_URL', 'builds.fotomatter.net');
