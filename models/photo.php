4d49fdec026dc249b7327d8f98508049215c723a
Andrew Morrill <acmorrill@gmail.com>
2014-08-19 17:41:52 -0600

    "Changes"

--- models/photo.php
+++ models/photo.php
@@ -460,7 +460,7 @@ class Photo extends AppModel {
 			'PhotoCache.photo_id' => $photo_id,
 			'PhotoCache.max_height' => $height,
 			'PhotoCache.max_width' => $width,
-			'PhotoCache.crop' => ($crop === true) ? 1 : 0,
+			'PhotoCache.crop' => ($crop === true) ? '1' : '0',
 		);
 
 
