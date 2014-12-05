d790cb1fd1c8374f664897ce4a08fd58e4861cb6
Andrew Morrill <acmorrill@gmail.com>
2014-12-05 15:18:27 -0700

    "lots of changes for the welcome pages"

--- controllers/welcome_controller.php
+++ controllers/welcome_controller.php
@@ -1,65 +1,69 @@
 <?php
+
 class WelcomeController extends AppController {
-    public $name = 'Welcome';
+
+	public $name = 'Welcome';
 	public $uses = array();
-	public $layout = 'admin/welcome';
-	
-	
-	public function  beforeFilter() {
-		
+	public $layout = 'admin/login';
+
+	public function beforeFilter() {
+
 		if (isset($_COOKIE['welcome_hash'])) {
 			$this->Auth->allow('admin_create_password', 'admin_index');
 		} else {
 			$this->Auth->allow('admin_index');
 		}
-		
- 		parent::beforeFilter();
+
+		parent::beforeFilter();
 	}
-	
+
 	public function admin_index($account_welcome_email_hash) {
 		$this->Welcome = ClassRegistry::init('Welcome');
-		
+
 		// check valid hash
 		$hash_valid = $this->Welcome->welcome_email_hash_is_valid($account_welcome_email_hash);
-	
+
 		if ($hash_valid === false) {
 			$this->Welcome->major_error('Welcome index with invalid hash', compact('account_welcome_email_hash'), 'low');
 			header('HTTP/1.0 404 Not Found');
 			exit();
 		}
-		
+
 		// check site built
 		$site_built = $this->Welcome->site_is_built($account_welcome_email_hash);
-		
+
+		///////////////////////////////////////////////////////////////////////////////////////
+		// go directly to the first step after build because site is already built
 		if ($site_built !== false) {
-			header("Location: /admin/welcome/create_password?wh=".$site_built);
+			header("Location: /admin/welcome/create_password?wh=" . $site_built);
 			exit();
 		}
-	
+
+		//////////////////////////////////////////////////////////////////////////////////////
+		// site not built so build the site via ajax on this page
 		$this->set(compact('account_welcome_email_hash', 'hash_valid', 'site_built'));
 	}
-	
-	
-    public function admin_create_password() {
-		
+
+	public function admin_create_password() {
+
 		$this->SiteSetting = ClassRegistry::init('SiteSetting');
-		
-		
+
+
 		// don't do this method if the password has already been set
 		if ($this->SiteSetting->getVal('welcome_password_set', 1) == 1) {
 			$this->redirect('/admin/welcome/choose_theme');
 		}
-		
-		
+
+
 		$account_email = $this->SiteSetting->getVal('account_email', false);
 		$this->set(compact('account_email'));
-		
+
 		if ($account_email === false) {
 			$this->SiteSetting->major_error('account_email not set for account');
 			$this->Session->setFlash(__('An error occured during site build. Please contact support.', true), 'admin/flashMessage/error');
 		}
-		
-		
+
+
 		// validate data if it was submitted
 		if (!empty($this->data['password']) && !empty($this->data['confirm_password'])) {
 			try {
@@ -69,8 +73,8 @@ class WelcomeController extends AppController {
 				$this->Session->setFlash($e->getMessage(), 'admin/flashMessage/error');
 				return;
 			}
-			
-			
+
+
 			$this->User = ClassRegistry::init('User');
 			if (($new_user_id = $this->User->create_user($account_email, $this->data['password'], true)) === false) {
 				// failed to create user
@@ -79,56 +83,55 @@ class WelcomeController extends AppController {
 				$this->Session->setFlash(__('An error occured during site build. Please contact support.', true), 'admin/flashMessage/error');
 				return;
 			}
-			
-			
+
+
 			// user created so mark welcome_password as having been done
 			$this->SiteSetting->setVal('welcome_password_set', 1);
-			
-			
+
+
 			// log the new user in
 			$this->Auth->login($new_user_id);
 			$this->redirect('/admin/welcome/choose_theme');
 		}
 	}
-	
-	
+
 	public function admin_choose_theme() {
 		$this->SiteSetting = ClassRegistry::init('SiteSetting');
-		
+
 		// don't do this method if the theme has already been chosen
 		if ($this->SiteSetting->getVal('welcome_theme_chosen', 1) == 1) {
 			$this->redirect('/admin/welcome/your_site');
 		}
-		
-		
+
+
 		if (!empty($this->data['new_theme_id'])) {
 			$this->Theme = ClassRegistry::init('Theme');
 			$this->Theme->change_to_theme_by_id($this->data['new_theme_id']);
-			
+
 			$this->SiteSetting->setVal('welcome_theme_chosen', 1);
-			
+
 			$this->redirect('/admin/welcome/your_site');
 		}
 	}
-	
-	
+
 //	public function admin_add_features() { // DREW TODO - maybe add this to the welcome
 //		exit('add features'); // DREW TODO - maybe give them a discount if they get a feature right away
 //	}
-	
-	
+
+
 	public function admin_your_site() {
 		// figure out the correct dns domain
 		$this->SiteSetting = ClassRegistry::init('SiteSetting');
 		$site_domain = $this->SiteSetting->getVal('site_domain', false);
-		
+
 		$dns_domain = '';
 		if ($site_domain === false) {
 			$this->SiteSetting->major_error('Site domain not set!');
 		} else {
-			$dns_domain = $site_domain.".fotomatter.net";
+			$dns_domain = $site_domain . ".fotomatter.net";
 		}
-		
+
 		$this->set(compact('dns_domain'));
 	}
+
 }
--- views/welcome/admin_index.ctp
+++ views/welcome/admin_index.ctp
@@ -1,27 +1,32 @@
 <script type="text/javascript">
 	$(document).ready(function() {
-		
-	console.log('here');
-		jQuery.ajax({
-				type : 'GET',
-				url : '//<?php echo Configure::read('OVERLORD_URL'); ?>/fm_build/signup_site_build/<?php echo $account_welcome_email_hash; ?>/?callback=?',
-				success: function(response) {
-					if (response.code) {
-						$(".build-pending h3").html("Site built successfully.");
-						setTimeout(function() {
-							window.location = '/admin/welcome/create_password?wh=' + response.data.welcome_hash;
-						}, 3000); 
-					} else {
-						$(".build-pending h3").html("Problem with site build");
-					}
+//		jQuery.ajax({
+//			type: 'GET',
+//			url: '//<?php echo Configure::read('OVERLORD_URL'); ?>/fm_build/signup_site_build/<?php echo $account_welcome_email_hash; ?>/?callback=?',
+//			success: function(response) {
+//				if (response.code) {
+//					$(".build-pending h3").html("Site built successfully.");
+//					setTimeout(function() {
+//						window.location = '/admin/welcome/create_password?wh=' + response.data.welcome_hash;
+//					}, 3000);
+//				} else {
+//					$(".build-pending h3").html("Problem with site build");
+//				}
+//
+//			},
+//			dataType: 'jsonp'
+//		});
 
-				},
-				
-				dataType: 'jsonp'
-	       });
+		jQuery('#site_building_progress').progressbar({ value: false });
 	});
 </script>
-<div style='text-align:center' class='build-pending'>
-	<h3>We are building one great site, please wait.</h3>
-	<img src='/img/admin/icons/ajax-loader.gif' alt="" />
+<div id="welcome_page_container">
+	<div class='generic_palette_container'>
+		<div class='fade_background_top'></div>
+		<?php echo $this->Session->flash('auth'); ?>
+		<div style='text-align: center' class='build-pending'>
+			<h1 style="margin-bottom: 35px;"><?php echo __('Your site will finish building momentarily', true); ?></h1>
+			<div id="site_building_progress"></div>
+		</div>
+	</div>
 </div>
