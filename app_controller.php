786d7b8c5f522aadb67e6e19315ae5b57cc211f9
Andrew Morrill <acmorrill@gmail.com>
2015-01-15 16:53:07 -0700

    "Changes"

--- app_controller.php
+++ app_controller.php
@@ -116,11 +116,7 @@ class AppController extends Controller {
 		$system_url = "$site_domain.fotomatter.net";
 		$on_system_site = $_SERVER['HTTP_HOST'] === $system_url;
 		$redirect_to_ssl = $in_admin || $in_checkout;
-		$server = $_SERVER;
-		$debug = Configure::read('debug');
-		$this->log(compact('in_admin', 'in_checkout', 'redirect_to_ssl', 'server', 'debug', 'on_system_site'), 'ssl_redirect');
 		if ( (empty($_SERVER['HTTPS']) || !$on_system_site) && Configure::read('debug') == 0 && $redirect_to_ssl) {
-			$this->log('came here 1', 'ssl_redirect');
 			$this->redirect("https://$site_domain.fotomatter.net{$_SERVER['REQUEST_URI']}");
 			exit();
 		}
@@ -135,10 +131,8 @@ class AppController extends Controller {
 		// 3) if don't need to redirect to ssl
 		$current_primary_domain = $this->AccountDomain->get_current_primary_domain();
 		$http_host = $_SERVER["HTTP_HOST"];
-//		$not_on_welcome_site = $this->not_on_welcome_site;
-//		$this->log(compact('current_primary_domain', 'http_host', 'not_on_welcome_site', 'redirect_to_ssl'), 'primary_domain_redirect');
 		if ($this->not_on_welcome_site && Configure::read('debug') == 0 && !$redirect_to_ssl && $http_host != $current_primary_domain) {
-			$this->redirect("http://$current_primary_domain");
+			$this->redirect("http://$current_primary_domain{$_SERVER['REQUEST_URI']}");
 			exit();
 		}
 		$this->set('current_primary_domain', $current_primary_domain);
