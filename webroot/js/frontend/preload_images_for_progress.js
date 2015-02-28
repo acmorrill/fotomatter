e94f1e13f16b1e60db0a52279274bc700883c29e
Andrew Morrill <acmorrill@gmail.com>
2015-02-27 23:03:00 -0700

    "Changes"

--- webroot/js/frontend/preload_images_for_progress.js
+++ webroot/js/frontend/preload_images_for_progress.js
@@ -1,6 +1,12 @@
+function sleep(miliseconds) {
+   var currentTime = new Date().getTime();
+
+   while (currentTime + miliseconds >= new Date().getTime()) {}
+}
 var total_images = 0;
 var loaded_images = 0;
 function update_progress_bar() {
+//	sleep(2000);
 	var total_progress = Math.round((loaded_images / total_images) * 100);
 	jQuery(document).trigger('image_load_progress', [ total_progress ]);
 	if (total_progress == 100) {
@@ -9,8 +15,11 @@ function update_progress_bar() {
 }
 jQuery(document).ready(function() {
 	jQuery(document).bind('preload_images_for_progress', function() {
-		jQuery('img.preload_for_progress').each(function() {
+		var preload_images = jQuery('img.preload_for_progress');
+		preload_images.each(function() {
 			total_images++;
+		});
+		preload_images.each(function() {
 			var img_src = $(this).attr('src');
 			var tmpImg = document.createElement('img'); // new Image(1, 1); 
 			tmpImg.src = img_src;
