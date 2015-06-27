4d1565ae355e05401ff486ae5fb5bf45a7fb8cf8
Andrew Morrill <acmorrill@gmail.com>
2015-06-26 22:04:20 -0600

    "Changes"

--- config/versioning/local/dev/0002/drew/0064.php
+++ config/versioning/local/dev/0002/drew/0064.php
@@ -4,27 +4,27 @@ $sqls = array();
 
 $functions = array();
 
-$functions[] = function() {
-	$SiteSetting = ClassRegistry::init('SiteSetting');
-	$User = ClassRegistry::init('User');
-	$account_email = $SiteSetting->getVal('account_email', '');
-	
-	if (!empty($account_email)) {
-		$users_to_delete = $User->find('all', array(
-			'conditions' => array(
-				'AND' => array(
-					'NOT' => array(
-						'User.email_address' => $account_email,
-					),
-					'User.admin' => 1,
-				),
-			),
-			'contain' => false,
-		));
-		foreach ($users_to_delete as $to_delete) {
-			$User->delete($to_delete['User']['id']);
-		}
-	}
-	
-	return true;
-};
+//$functions[] = function() {
+//	$SiteSetting = ClassRegistry::init('SiteSetting');
+//	$User = ClassRegistry::init('User');
+//	$account_email = $SiteSetting->getVal('account_email', '');
+//	
+//	if (!empty($account_email)) {
+//		$users_to_delete = $User->find('all', array(
+//			'conditions' => array(
+//				'AND' => array(
+//					'NOT' => array(
+//						'User.email_address' => $account_email,
+//					),
+//					'User.admin' => 1,
+//				),
+//			),
+//			'contain' => false,
+//		));
+//		foreach ($users_to_delete as $to_delete) {
+//			$User->delete($to_delete['User']['id']);
+//		}
+//	}
+//	
+//	return true;
+//};
--- config/versioning/local/dev/0002/drew/0065.php
+++ config/versioning/local/dev/0002/drew/0065.php
@@ -4,7 +4,7 @@ $sqls = array();
 
 $functions = array();
 
-//$functions[] = function() {
+//$functions[] = function() { // this does not work on build
 //	$User = ClassRegistry::init('User');
 //	$User->create_user("support@fotomatter.net", '0pxYyDb3nFAbzJS5kdwmXLv0tBdDzf', true);
 //		
