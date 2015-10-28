828d05067b6a0b71e9c581dcf56239066947ad22
Andrew Morrill <acmorrill@gmail.com>
2015-10-27 20:41:43 -0600

    "Changes"

--- config/versioning/local/dev/0002/drew/0077.php
+++ config/versioning/local/dev/0002/drew/0077.php
@@ -4,10 +4,5 @@ $sqls = array();
 
 $functions = array();
 
-$sqls[] = "ALTER TABLE  `photos` CHANGE  `cdn-filename`  `cdn-filename` CHAR( 128 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;";
-$sqls[] = "ALTER TABLE  `photos` CHANGE  `cdn-filename-forcache`  `cdn-filename-forcache` CHAR( 128 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;";
-$sqls[] = "ALTER TABLE  `photos` CHANGE  `cdn-filename-smaller-forcache`  `cdn-filename-smaller-forcache` CHAR( 128 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;";
-$sqls[] = "ALTER TABLE  `photo_caches` CHANGE  `cdn-filename`  `cdn-filename` CHAR( 128 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;";
-
 $sqls[] = "ALTER TABLE `photo_galleries` ADD INDEX `weight` (`weight`)";
 $sqls[] = "ALTER TABLE `photo_galleries_photos` ADD INDEX `photo_order` (`photo_gallery_id`, `photo_order`)";
