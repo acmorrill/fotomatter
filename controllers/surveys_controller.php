92f4680bb292a54f3791f3fe5f26a2aa76681015
Josh Rees <josh.d.rees@gmail.com>
2016-02-22 22:27:13 -0700

    added account_id

--- controllers/surveys_controller.php
+++ controllers/surveys_controller.php
@@ -6,9 +6,11 @@ class SurveysController extends AppController {
 	public $layout = 'admin/sidebar_less';
 
 	public function admin_index() {
+		$this->SiteSetting = ClassRegistry::init('SiteSetting');
+		$account_id = $this->SiteSetting->getVal('account_id', false);
 		$curr_page = 'site_settings';
 		$curr_sub_page = 'surveys';
 		$title_for_layout = 'Surveys';
-		$this->set(compact(array('debugging', 'curr_page', 'curr_sub_page', 'title_for_layout')));
+		$this->set(compact(array('debugging', 'curr_page', 'curr_sub_page', 'title_for_layout', 'account_id')));
 	}
 }
